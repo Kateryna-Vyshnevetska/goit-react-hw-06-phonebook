ddc12bee967215f00adbe2cb35f39f2cdbb041c6
Kateryna Vyshnevetska <vyshnevetska.k@gmail.com>
2020-09-20 11:54:09 +0300

    deploy

---
+++ build/precache-manifest.d1340c2beb781c2a27b22ed1affcac61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d0228f330fab0bc77ad1221d30e86f",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "7367db84f0c927698ccb",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.e963f0be.chunk.css"
+  },
+  {
+    "revision": "4c5f42a9c57e8d14b04e",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.3e57af77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.3e57af77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7367db84f0c927698ccb",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.ddddc868.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-05-phonebook/precache-manifest.0c6042868e30392a6114dc6e97e46bbe.js"
+  "/goit-react-hw-06-phonebook/precache-manifest.d1340c2beb781c2a27b22ed1affcac61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-05-phonebook/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-06-phonebook/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
